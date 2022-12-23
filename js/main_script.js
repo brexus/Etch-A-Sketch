6aa9ca328bbf9ea4c8d2302a48b7213a1fa6dec6
brexus <92671269+brexus@users.noreply.github.com>
2022-12-23 22:02:32 +0100

    added new buttons

--- js/main_script.js
+++ js/main_script.js
@@ -1,6 +1,7 @@
 let grid_container = document.querySelector('#grid-container');
+let color = 'black';
 
-function grid_generator(size) {
+function grid_generator(size=16) {
 
     for(let i = 0; i < size*size; i++) {
         grid_container.classList.remove('grid16','grid32','grid64');
@@ -9,7 +10,7 @@ function grid_generator(size) {
         cell.classList.add('cell');
 
         cell.addEventListener('mouseenter', function(e) {
-            cell.style.backgroundColor = "black";
+            cell.style.backgroundColor = color;
         });
 
         grid_container.classList.add(`grid${size}`);
@@ -32,20 +33,32 @@ function erase_listener() {
 }
 
 function button_listener() {
-    let buttons = document.querySelectorAll('[data-key]');
+    let res_buttons = document.querySelectorAll('[data-key]');
     let size;
 
     for(let i = 0; i < 3; i++){
-        buttons[i].addEventListener('click', function(e) {
-            console.log('clicked '+ i);
+        res_buttons[i].addEventListener('click', function(e) {
             if(i == 0) size = Number(16);
             else if(i == 1) size = Number(32);
             else if(i == 2) size = Number(64);
             grid_generator(size);
         });
     }
+
+    let color_buttons = document.querySelectorAll('#color_button[data-key]');
+    console.log(color_buttons);
+
+    for(let i = 0; i < 2; i++){
+        color_buttons[i].addEventListener('click', function(e) {
+            if(i == 0) class_ = 'normal';
+            else if(i == 1) class_ = 'psycho';
+            console.log("color button " + i);
+            
+        });
+    }
 }
+
 
 button_listener();
 erase_listener();
-grid_generator(16);
+grid_generator();
